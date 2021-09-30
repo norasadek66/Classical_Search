1a711d26b5171c987f38d5467b7546bc772151aa
norasadek66 <36291584+norasadek66@users.noreply.github.com>
2021-09-30 19:07:22 +0200

    Replacing dicitionaries with arrays actings as queue

--- BFS.py
+++ BFS.py
@@ -28,11 +28,12 @@ class Graph(object):
 
 def bfs_search(root_node, search_value):
     node_list = []
-    explored = {}
-    frontier = {}
+    explored = []
+    index=0
+    frontier = []
     edge = 0
     to_be_checked_node = root_node
-    print('here', root_node.value)
+    #print('here', root_node.value)
     graph_result = Graph(node_list)
     #print (graph_result.nodes, "check")
     if root_node.value == search_value:
@@ -40,33 +41,45 @@ def bfs_search(root_node, search_value):
         graph_result = Graph(node_list)
         return graph_result
     else:
+        #graph_result.nodes=[root_node]
+        #print graph_result.nodes[0].value, "first element"
         while True:
-            explored[to_be_checked_node] = edge
-            children = to_be_checked_node.children
-            edge += 1
-            for child in children:
-                if child not in list(frontier.keys()) and child not in list(explored.keys()):
-                    frontier[child] = edge
-                    for key in frontier.keys():
-                        first_child = key
-                        break
+            if to_be_checked_node not in explored:
+                explored.append(to_be_checked_node)
+                #print to_be_checked_node.value, "following element"
+                children = to_be_checked_node.children
+                for child in children:
+                    if child not in (frontier) and child not in (explored):
+                        #print"child ", child.value
+                        frontier.append(child)
 
+                for key in frontier:
+                    #print 'firsttt ', key.value
+                    first_child = key
+                    break
+                if first_child.value == search_value:
+                    #print "bingo", explored[1].value
+                    explored.append(first_child)
+                    keys_list = []
+                    for key in explored:
+                        #print key.value, "key values here"
+                        keys_list.append(key)
+                    #print len(keys_list), "length"
+                    for i, key in enumerate(keys_list):
+                        if i != (len(keys_list) - 1):
+                            print "following key ", key.value
+                            graph_result.nodes.append(key)
+                            #graph_result.add_edge(key, keys_list[i + 1])
+                        else:
+                            graph_result.nodes.append(key)
+                    #temp = list(explored)
 
-            if first_child.value == search_value:
-                print('bingo')
-                keys_list = list(explored)
-                graph_result.nodes+=(keys_list)
-                for i, key in enumerate(keys_list):
-                    if i != (len(keys_list) - 1):
-                        graph_result.add_edge(key, keys_list[i + 1])
-                temp = list(explored)
-                return graph_result
-            else:
-                #print(first_child, 'first child')
-                #print ( frontier.get(first_child), "original first child")
-                frontier.pop(first_child)
-                #del frontier[first_child]
-                to_be_checked_node = first_child
+                    return graph_result
+                else:
+
+                    frontier.pop(0)
+                    to_be_checked_node = first_child
+
 
 
 
@@ -90,9 +103,11 @@ if __name__ == "__main__":
     graph1.add_edge(nodeH, nodeG)
     graph1.add_edge(nodeH, nodeP)
     graph1.add_edge(nodeS, nodeR)
-    #Graph_Result= bfs_search(nodeS, 'A')
-    graph_result2 = bfs_search(nodeP, 'S')
-    #print (Graph_Result.nodes[1].value)
+    Graph_Result= bfs_search(nodeS, 'A')
+    #graph_result2 = bfs_search(nodeP, 'S')
+    #print (graph_result2.nodes[1].value)
+    for i in range(len(Graph_Result.nodes)):
+        print Graph_Result .nodes[i].value
 
 
 
